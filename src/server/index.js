21187fc0d9b554cdeea3949afee23e93715c8add
windlil <spacesouls@163.com>
2023-08-07 16:15:11 +0800

    chore: init webpack config

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,13 @@
 const express = require('express')
+
+let server  = express()
+
+server.get('/', (req, res) => {
+  res.send(
+    `hello node serve`
+  )
+})
+
+server.listen(3000, () => {
+  console.log("start node server on 3000")
+})
